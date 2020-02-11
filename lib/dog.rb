50bad9a5f18d7534713fe17bfc39fa98dfe5cd93
Matoya Williams <matoyawilliams@yahoo.com>
2020-02-11 21:33:22 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog 
+  def name=(dogs_name)
+    @name = dogs_name
+    
+  def breed=(dogs_breed)
+    @breed = dogs_breed
+end 
+
+def name 
+  @name
+  
+  def breed
+    @breed 
+end 
+end 
---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person 
+  def name=(person_name)
+    @name = dogs_name
+    
+  def breed=(dogs_breed)
+    @breed = dogs_breed
+end 
+
+def name 
+  @name
+  
+  def breed
+    @breed 
+end 
+end 
