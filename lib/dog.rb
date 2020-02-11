4757a14879eb9dc12a888119515484a50f278fca
Matoya Williams <matoyawilliams@yahoo.com>
2020-02-11 21:42:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,17 @@
-class Dog 
+class Dog
+  
   def name=(dogs_name)
     @name = dogs_name
- end    
-  def breed=(dogs_breed)
-    @breed = dogs_breed
-end 
-
-def name 
-  @name
-def breed
-    @breed 
-end 
-end 
-
-
-class Dog
+  end
+  
+  def name
+    @name 
+  end
+  def breed=(breed)
+    @breed = breed
+  end
+  
+  def breed
+    @breed
+  end 
+end
