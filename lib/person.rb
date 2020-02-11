b8eaa341a9010f848c921b368750111c9ed4394b
Matoya Williams <matoyawilliams@yahoo.com>
2020-02-11 21:39:55 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
   def name= (Ada)
     @name = "Ada"
+  def job= (teacher)
+    @name = "teacher"
+  end
+  
+  def name
+    @name
+  end
+  def job
+    @job
+  end
+end
+
+ada= Person.new 
+ada.name
+ada.job
