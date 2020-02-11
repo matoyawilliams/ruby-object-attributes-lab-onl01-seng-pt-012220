00ff2b27a84d84537eaf2c04bfbcaeed70361425
Matoya Williams <matoyawilliams@yahoo.com>
2020-02-11 21:42:52 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -18,20 +18,3 @@ class Person
   
 end
 
-# Notes and practice 
-
-class Record
-  
-  def genre=(records_genre)
-    @grenre = records_genre
-  end 
-  
-  def genre
-    @genre 
-  end 
-  
-end 
-
-beasty_boys = Record.new
-beasty_boys.genre = "Rap and R&B"
-beasty_boys.gerne 
